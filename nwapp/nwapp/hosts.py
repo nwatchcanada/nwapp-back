9037ce3aa3ca6b7912eb6e7ebf87916cb8c1ad3a
bartmika <bart@mikasoftware.com>
2019-05-29 15:12:58 -0400

    BUGFIX: Fixed so landpage does not require `www` subdomain.

--- nwapp/nwapp/hosts.py
+++ nwapp/nwapp/hosts.py
@@ -3,7 +3,8 @@ from django_hosts import patterns, host
 
 
 host_patterns = patterns('',
-    host(r'www', settings.ROOT_URLCONF, name='www'),  # <-- The `name` we used to in the `DEFAULT_HOST` setting
+    host(None, settings.ROOT_URLCONF, name="index"),
+    host(r'www', settings.ROOT_URLCONF, name='www'),
     host(r'admin', settings.ROOT_URLCONF, name='admin'),
     # host(r'(\w+)', 'tenant_district.urls', name='tenant_district'),
     host(r'(\w+)', 'api.urls', name='wildcard'),
