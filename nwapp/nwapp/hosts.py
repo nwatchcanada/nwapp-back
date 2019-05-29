b69606caf6f8f6712853555980f39ecd4602dc3c
bartmika <bart@mikasoftware.com>
2019-05-29 15:23:10 -0400

    Updated `hosts.py` file.

--- nwapp/nwapp/hosts.py
+++ nwapp/nwapp/hosts.py
@@ -5,7 +5,5 @@ from django_hosts import patterns, host
 host_patterns = patterns('',
     host(None, settings.ROOT_URLCONF, name="index"),
     host(r'www', settings.ROOT_URLCONF, name='www'),
-    host(r'admin', settings.ROOT_URLCONF, name='admin'),
-    # host(r'(\w+)', 'tenant_district.urls', name='tenant_district'),
     host(r'(\w+)', 'api.urls', name='wildcard'),
 )
