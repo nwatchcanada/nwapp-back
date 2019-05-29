ba9b4065cac761c492f476ce7daef7039a6fe060
bartmika <bart@mikasoftware.com>
2019-05-29 19:12:48 -0400

    Removed `www` subdomain.

--- nwapp/nwapp/hosts.py
+++ nwapp/nwapp/hosts.py
@@ -3,7 +3,6 @@ from django_hosts import patterns, host
 
 
 host_patterns = patterns('',
-    host(None, settings.ROOT_URLCONF, name="index"),
-    host(r'www', settings.ROOT_URLCONF, name='www'),
+    host(None, settings.ROOT_URLCONF, name="www"),
     host(r'(\w+)', 'api.urls', name='wildcard'),
 )
