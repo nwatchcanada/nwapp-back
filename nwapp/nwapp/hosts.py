0e0731702c640fedeed9de03f1140436df301654
bartmika <bart@mikasoftware.com>
2019-05-29 20:01:19 -0400

    Minor edit.

--- nwapp/nwapp/hosts.py
+++ nwapp/nwapp/hosts.py
@@ -3,6 +3,7 @@ from django_hosts import patterns, host
 
 
 host_patterns = patterns('',
-    host(None, settings.ROOT_URLCONF, name="www"),
+    host(None, settings.ROOT_URLCONF, name="none"),
+    host(r'www', settings.ROOT_URLCONF, name="www"),
     host(r'(\w+)', 'api.urls', name='wildcard'),
 )
