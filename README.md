29f80d59227852e036d03bce63fd016b007bb5ec
bartmika <bart@mikasoftware.com>
2019-07-17 18:17:28 -0400

    Added more test account

--- README.md
+++ README.md
@@ -88,6 +88,11 @@ This project uses the ``PostGres`` database and as a result requires setup befor
   python manage.py create_admin_user "bart@mikasoftware.com" "123password" "Bart" "Mika";
   python manage.py setup_resource_server_authorization
   python manage.py collectstatic
+  python manage.py create_regular_user "london" "bart+management@mikasoftware.com" "123password" "Bart" "Mika" 2;
+  python manage.py create_regular_user "london" "bart+staff@mikasoftware.com" "123password" "Bart" "Mika" 3;
+  python manage.py create_regular_user "london" "bart+associate@mikasoftware.com" "123password" "Bart" "Mika" 4;
+  python manage.py create_regular_user "london" "bart+area@mikasoftware.com" "123password" "Bart" "Mika" 5;
+  python manage.py create_regular_user "london" "bart+member@mikasoftware.com" "123password" "Bart" "Mika" 6;
   ```
 
 2. Register the app with the following social-auth services. Also read [this tutorial](https://simpleisbetterthancomplex.com/tutorial/2016/10/24/how-to-add-social-login-to-django.html) on setting these up.
--- nwapp/shared_foundation/management/commands/create_regular_user.py
+++ nwapp/shared_foundation/management/commands/create_regular_user.py
@@ -2,13 +2,10 @@
 from django.conf import settings
 from django.contrib.auth.models import Group
 from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
 from django.utils.translation import ugettext_lazy as _
-from starterkit.utils import (
-    get_random_string,
-    get_unique_username_from_email
-)
 from shared_foundation import constants
-from shared_foundation.models import SharedUser
+from shared_foundation.models import SharedOrganization, SharedUser, SharedGroup
 
 
 class Command(BaseCommand):
@@ -17,27 +14,40 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         """
         Run manually in console:
-        python manage.py create_regular_user "bart+regular@mikasoftware.com" "123password" "Bart" "Mika";
+        python manage.py create_regular_user "london" "bart+regular@mikasoftware.com" "123password" "Bart" "Mika" 6;
         """
+        parser.add_argument('tenant_schema', nargs='+', type=str)
         parser.add_argument('email', nargs='+', type=str)
         parser.add_argument('password', nargs='+', type=str)
         parser.add_argument('first_name', nargs='+', type=str)
         parser.add_argument('last_name', nargs='+', type=str)
+        parser.add_argument('group_membership', nargs='+', type=int)
 
+    @transaction.atomic
     def handle(self, *args, **options):
         # Get the user inputs.
+        tenant_schema = options['tenant_schema'][0]
         email = options['email'][0]
         password = options['password'][0]
         first_name = options['first_name'][0]
         last_name = options['last_name'][0]
+        group_membership_id = options['group_membership'][0]
+
+        tenant = SharedOrganization.objects.filter(schema=tenant_schema).first()
+        if tenant is None:
+            raise CommandError(_('Organization could not be found.'))
 
         # Defensive Code: Prevent continuing if the email already exists.
         if SharedUser.objects.filter(email=email).exists():
             raise CommandError(_('Email already exists, please pick another email.'))
 
+        group_membership = SharedGroup.objects.filter(id=group_membership_id).first()
+        if group_membership is None:
+            raise CommandError(_('Group could not be found.'))
+
         # Create the user.
         user = SharedUser.objects.create(
-            tenant=1,
+            tenant=tenant,
             first_name=first_name,
             last_name=last_name,
             email=email,
@@ -52,6 +62,8 @@ class Command(BaseCommand):
             shipping_last_name = last_name,
             shipping_email = email
         )
+
+        user.groups.add(group_membership)
 
         # Generate and assign the password.
         user.set_password(password)
