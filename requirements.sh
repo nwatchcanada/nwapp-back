89d7d471efbedcf04c8bad2224b6e8d627657bad
bartmika <bart@mikasoftware.com>
2019-07-22 17:37:01 -0400

    Removed file. Created new file

---
+++ requirements.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+go get -u github.com/go-chi/chi
+go get -u github.com/go-chi/chi/middleware
+go get -u github.com/go-chi/render
+go get -u github.com/go-chi/jwtauth
+go get -u github.com/go-chi/valve
+go get -u github.com/joho/godotenv        # Environment Variable Loader
+go get -u github.com/lib/pq               # Postgres DB Driver for Golang
+go get -u github.com/jmoiron/sqlx         # General purpose extensions to golang's database/sql
+go get -u golang.org/x/crypto/bcrypt      # Bycrypt Hashing Algorithm
+go get -u github.com/spf13/cobra/cobra    # Modern Go CLI interactions
+go get -u github.com/dgrijalva/jwt-go     # JWT Library
