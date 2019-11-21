1d6c644f603519792e6fe9f288757e771d67c177
bartmika <bart@mikasoftware.com>
2019-11-21 12:14:24 -0500

    Seperated into contact section. #9

--- nwapp/tenant_foundation/models/__init__.py
+++ nwapp/tenant_foundation/models/__init__.py
@@ -4,4 +4,5 @@ from tenant_foundation.models.how_hear_about_us_item import HowHearAboutUsItem
 from tenant_foundation.models.tag import Tag
 from tenant_foundation.models.member import Member
 from tenant_foundation.models.member_address import MemberAddress
+from tenant_foundation.models.member_contact import MemberContact
 from tenant_foundation.models.member_metric import MemberMetric
--- nwapp/tenant_foundation/models/member_contact.py
+++ nwapp/tenant_foundation/models/member_contact.py
@@ -16,9 +16,9 @@ from shared_foundation.models import SharedUser
 email_validator = EmailValidator(message=_("Invalid email"))
 
 
-class MemberManager(models.Manager):
+class MemberContactManager(models.Manager):
     def delete_all(self):
-        items = Member.objects.iterator(chunk_size=50)
+        items = MemberContact.objects.iterator(chunk_size=50)
         for item in items.all():
             item.delete()
 
@@ -28,7 +28,7 @@ class MemberManager(models.Manager):
 
     def partial_text_search(self, keyword):
         """Function performs partial text search of various textfields."""
-        return Member.objects.filter(
+        return MemberContact.objects.filter(
             Q(indexed_text__icontains=keyword) |
             Q(indexed_text__istartswith=keyword) |
             Q(indexed_text__iendswith=keyword) |
@@ -42,10 +42,10 @@ class MemberManager(models.Manager):
         # which comes with Django to utilize the 'full text search' feature.
         # For more details please read:
         # https://docs.djangoproject.com/en/2.0/ref/contrib/postgres/search/
-        return Member.objects.annotate(search=SearchVector('indexed_text'),).filter(search=keyword)
+        return MemberContact.objects.annotate(search=SearchVector('indexed_text'),).filter(search=keyword)
 
 
-class Member(models.Model):
+class MemberContact(models.Model):
     """
     Class model represents the Neighbourhood Watch registered member for the
     particular tenant.
@@ -57,9 +57,9 @@ class Member(models.Model):
 
     class Meta:
         app_label = 'tenant_foundation'
-        db_table = 'nwapp_members'
-        verbose_name = _('Member')
-        verbose_name_plural = _('Members')
+        db_table = 'nwapp_member_contacts'
+        verbose_name = _('Member Contact')
+        verbose_name_plural = _('Member Contacts')
         default_permissions = ()
         permissions = ()
 
@@ -67,65 +67,15 @@ class Member(models.Model):
     CONSTANTS
     '''
 
-    class MEMBER_STATE:
-        ACTIVE = 'active'
-        INACTIVE = 'inactive'
-
-    class MEMBER_TYPE_OF:
-        BUSINESS = 'business'
-        RESIDENTIAL = 'residential'
-        COMMUNITY_CARES = 'community_cares'
-
-    class MEMBER_DEACTIVATION_REASON:
-        NOT_SPECIFIED = 0
-        OTHER = 1
-        BLACKLISTED = 2
-        MOVED = 3
-        DECEASED = 4
-        DO_NOT_CONTACT = 5
-
-    class MEMBER_ORGANIZATION_TYPE_OF:
-        PRIVATE = 'private'
-        GOVERNMENT = 'government'
-        NON_PROFIT = 'non_profit'
-        UNSPECIFIED = 'unspecified'
-
     '''
     CHOICES
     '''
-
-    MEMBER_STATE_CHOICES = (
-        (MEMBER_STATE.ACTIVE, _('Active')),
-        (MEMBER_STATE.INACTIVE, _('Inactive')),
-    )
-
-    MEMBER_TYPE_OF_CHOICES = (
-        (MEMBER_TYPE_OF.BUSINESS, _('Business')),
-        (MEMBER_TYPE_OF.RESIDENTIAL, _('Residential')),
-        (MEMBER_TYPE_OF.COMMUNITY_CARES, _('Community Cares')),
-    )
-
-    MEMBER_DEACTIVATION_REASON_CHOICES = (
-        (MEMBER_DEACTIVATION_REASON.BLACKLISTED, _('Blacklisted')),
-        (MEMBER_DEACTIVATION_REASON.MOVED, _('Moved')),
-        (MEMBER_DEACTIVATION_REASON.DECEASED, _('Deceased')),
-        (MEMBER_DEACTIVATION_REASON.DO_NOT_CONTACT, _('Do not contact')),
-        (MEMBER_DEACTIVATION_REASON.NOT_SPECIFIED, _('Not specified')),
-        (MEMBER_DEACTIVATION_REASON.OTHER, _('Other')),
-    )
-
-    MEMBER_ORGANIZATION_TYPE_OF_CHOICES = (
-        (MEMBER_ORGANIZATION_TYPE_OF.PRIVATE, _('Private')),
-        (MEMBER_ORGANIZATION_TYPE_OF.GOVERNMENT, _('Government')),
-        (MEMBER_ORGANIZATION_TYPE_OF.NON_PROFIT, _('Non-Profit')),
-        (MEMBER_ORGANIZATION_TYPE_OF.UNSPECIFIED, _('Unspecified')),
-    )
 
     '''
     OBJECT MANAGERS
     '''
 
-    objects = MemberManager()
+    objects = MemberContactManager()
 
     '''
     MODEL FIELDS
@@ -133,74 +83,58 @@ class Member(models.Model):
 
     # SYSTEM FIELDS
 
-    user = models.ForeignKey(
-        SharedUser,
-        help_text=_('The user whom owns this thing.'),
+    member = models.OneToOneField(
+        "Member",
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    is_ok_to_email = models.BooleanField(
+        _("Is OK to email"),
+        help_text=_('Indicates whether member allows being reached by email'),
+        default=True,
+        blank=True
+    )
+    is_ok_to_text = models.BooleanField(
+        _("Is OK to text"),
+        help_text=_('Indicates whether member allows being reached by text.'),
+        default=True,
+        blank=True
+    )
+    first_name = models.CharField(
+        _("First Name"),
+        max_length=63,
+        help_text=_('The member\'s given name.'),
         blank=True,
         null=True,
-        related_name="%(app_label)s_%(class)s_abstract_thing_owner_related",
-        on_delete=models.CASCADE
+        db_index=False,
     )
-    indexed_text = models.CharField(
-        _("Indexed Text"),
-        max_length=511,
-        help_text=_('The searchable content text used by the keyword searcher function.'),
+    last_name = models.CharField(
+        _("Last Name"),
+        max_length=63,
+        help_text=_('The member\'s sure name.'),
         blank=True,
         null=True,
-        db_index=True,
-        unique=True
+        db_index=False,
     )
-    type_of = models.PositiveSmallIntegerField(
-        _("Type of"),
-        help_text=_('The type of member this is.'),
-        choices=MEMBER_TYPE_OF_CHOICES,
-        db_index=True,
+    email = models.EmailField(
+        _("Email"),
+        help_text=_('Email address.'),
+        null=True,
+        blank=True,
+        validators=[email_validator],
+        db_index=True
     )
-
-    # ORGANIZATION FIELDS
-
-    organization_name = models.CharField(
-        _("Organization Name"),
-        max_length=255,
-        help_text=_('The name of the organization or business this person represents.'),
+    primary_phone = PhoneNumberField(
+        _("Primary Telephone"),
+        help_text=_('The primary telephone number used by the member.'),
         blank=True,
         null=True,
     )
-    organization_type_of = models.PositiveSmallIntegerField(
-        _("Organization Type of"),
-        help_text=_('The type of organization this is based on Neighbourhood Watch Canada internal classification.'),
-        default=MEMBER_ORGANIZATION_TYPE_OF.UNSPECIFIED,
+    secondary_phone = PhoneNumberField(
+        _("Secondary Telephone"),
+        help_text=_('The secondary or other telephone number used by the member.'),
         blank=True,
-        choices=MEMBER_ORGANIZATION_TYPE_OF_CHOICES,
-    )
-
-    # WATCH
-    #TODO: IMPLEMENT
-
-    # STATE FIELDS
-
-    state = models.CharField(
-        _('State'),
-        help_text=_('The state of this member.'),
-        max_length=15,
-        choices=MEMBER_STATE_CHOICES,
-        default=MEMBER_STATE.ACTIVE,
-        blank=True,
-        db_index=True,
-    )
-    deactivation_reason = models.PositiveSmallIntegerField(
-        _("Deactivation reason"),
-        help_text=_('The reason why this member was deactivated.'),
-        blank=True,
-        choices=MEMBER_DEACTIVATION_REASON_CHOICES,
-        default=MEMBER_DEACTIVATION_REASON.NOT_SPECIFIED
-    )
-    deactivation_reason_other = models.CharField(
-        _("Deactivation reason (other)"),
-        max_length=2055,
-        help_text=_('The reason why this member was deactivated which was not in the list.'),
-        blank=True,
-        default=""
+        null=True,
     )
 
     # AUDITING FIELDS
@@ -208,7 +142,7 @@ class Member(models.Model):
     created_by = models.ForeignKey(
         SharedUser,
         help_text=_('The user whom created this object.'),
-        related_name="created_members",
+        related_name="created_member_contacts",
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
@@ -228,7 +162,7 @@ class Member(models.Model):
     last_modified_by = models.ForeignKey(
         SharedUser,
         help_text=_('The user whom modified this object last.'),
-        related_name="last_modified_members",
+        related_name="last_modified_member_contacts",
         on_delete=models.SET_NULL,
         blank=True,
         null=True,
@@ -295,40 +229,10 @@ class Member(models.Model):
         If we are creating a new model, then we will automatically increment the `id`.
         '''
         if self.id == 0 or self.id == None:
-            self.id = Member.objects.count() + 1
+            self.id = MemberContact.objects.count() + 1
 
         '''
         Finally call the parent function which handles saving so we can carry
         out the saving operation by Django in our ORM.
         '''
-        super(Member, self).save(*args, **kwargs)
-
-    def get_pretty_state(self):
-        return dict(MEMBER_STATE_CHOICES).get(self.state)
-
-    def get_pretty_type_of(self):
-        return dict(MEMBER_TYPE_OF_CHOICES).get(self.type_of)
-
-    def get_pretty_deactivation_reason(self):
-        return dict(MEMBER_DEACTIVATION_REASON_CHOICES).get(self.deactivation_reason)
-
-    def get_pretty_organization_type_of(self):
-        return dict(MEMBER_ORGANIZATION_TYPE_OF_CHOICES).get(self.organization_type_of)
-
-
-
-    def invalidate(self, method_name):
-        """
-        Function used to clear the cache for the cached property functions.
-        """
-        try:
-            pass
-            # if method_name == 'fullname':
-            #     del self.fullname
-            # else:
-            #     raise Exception("Method name not found.")
-        except AttributeError:
-            pass
-
-    def invalidate_all(self):
-        self.invalidate("fullname")
+        super(MemberContact, self).save(*args, **kwargs)
