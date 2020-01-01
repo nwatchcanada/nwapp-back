19021505ff46bd25a3f0e3351390f5474ee3c41f
bartmika <bart@mikasoftware.com>
2019-12-31 22:45:36 -0500

    Implemented latest code for the area coordinator promotion.

--- nwapp/tenant_member/serializers/promote_operation_serializers.py
+++ nwapp/tenant_member/serializers/promote_operation_serializers.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import Group
 from django.contrib.auth import authenticate
 from django.db import transaction
 from django.db.models import Q, Prefetch
+from django.template.loader import render_to_string  # HTML / TXT
 from django.utils.translation import ugettext_lazy as _
 from django.utils import timezone
 from django.utils.http import urlquote
@@ -14,12 +15,14 @@ from rest_framework import exceptions, serializers
 from rest_framework.response import Response
 from rest_framework.validators import UniqueValidator
 
-from shared_foundation.models import SharedUser
+from shared_foundation.models import SharedUser, SharedGroup
 from tenant_foundation.constants import *
 from tenant_foundation.models import (
     Comment,
     Member,
-    MemberComment
+    MemberComment,
+    AreaCoordinator,
+    Associate
 )
 
 
@@ -36,19 +39,87 @@ class MemberPromoteOperationSerializer(serializers.Serializer):
     associate_agreement = serializers.BooleanField(write_only=True, required=True,)
     police_check_date = serializers.DateField(write_only=True, required=True,)
 
+    def create_area_coordinator(self, validated_data):
+        slug = validated_data.get('member')
+        member = Member.objects.select_for_update().get(user__slug=slug)
+        request = self.context.get('request')
+        area_coordinator_agreement = validated_data.get('area_coordinator_agreement')
+        conflict_of_interest_agreement = validated_data.get('conflict_of_interest_agreement')
+        code_of_conduct_agreement = validated_data.get('code_of_conduct_agreement')
+        confidentiality_agreement = validated_data.get('confidentiality_agreement')
+        # associate_agreement = validated_data.get('associate_agreement')
+        police_check_date = validated_data.get('police_check_date')
+
+        logger.info("Beginning promotion...")
+
+        # Get the text agreement which will be signed.
+        area_coordinator_agreement = render_to_string('account/area_coordinator_agreement/2019_05_01.txt', {})
+        conflict_of_interest_agreement = render_to_string('account/conflict_of_interest_agreement/2019_05_01.txt', {})
+        code_of_conduct_agreement = render_to_string('account/code_of_conduct_agreement/2019_05_01.txt', {})
+        confidentiality_agreement = render_to_string('account/confidentiality_agreement/2019_05_01.txt', {})
+        # associate_agreement = render_to_string('account/associate_agreement/2019_05_01.txt', {})
+
+        # Create or update our model.
+        area_coordinator = AreaCoordinator.objects.update_or_create(
+            member=member,
+            defaults={
+                'member': member,
+                'has_signed_area_coordinator_agreement': True,
+                'area_coordinator_agreement': area_coordinator_agreement,
+                'area_coordinator_agreement_signed_on': timezone.now(),
+                'has_signed_conflict_of_interest_agreement': True,
+                'conflict_of_interest_agreement': conflict_of_interest_agreement,
+                'conflict_of_interest_agreement_signed_on': timezone.now(),
+                'has_signed_code_of_conduct_agreement': True,
+                'code_of_conduct_agreement': code_of_conduct_agreement,
+                'code_of_conduct_agreement_signed_on': timezone.now(),
+                'has_signed_confidentiality_agreement': True,
+                'confidentiality_agreement': confidentiality_agreement,
+                'confidentiality_agreement_signed_on': timezone.now(),
+                'police_check_date': police_check_date,
+            }
+        )
+
+        # Set the user's role to be a area coordinator after clearing the
+        # previous group memberships.
+        member.user.groups.clear()
+        member.user.groups.add(SharedGroup.GROUP_MEMBERSHIP.AREA_COORDINATOR)
+
+        # raise serializers.ValidationError({ # Uncomment when not using this code but do not delete!
+        #     "error": "Terminating for debugging purposes only."
+        # })
+
+        return area_coordinator
+
+    def create_associate(self, validated_data):
+        slug = validated_data.get('member')
+        member = Member.objects.select_for_update().get(user__slug=slug)
+        request = self.context.get('request')
+        area_coordinator_agreement = validated_data.get('area_coordinator_agreement')
+        conflict_of_interest_agreement = validated_data.get('conflict_of_interest_agreement')
+        code_of_conduct_agreement = validated_data.get('code_of_conduct_agreement')
+        confidentiality_agreement = validated_data.get('confidentiality_agreement')
+        associate_agreement = validated_data.get('associate_agreement')
+        police_check_date = validated_data.get('police_check_date')
+
+        logger.info("Beginning promotion...")
+
+        raise serializers.ValidationError({ # Uncomment when not using this code but do not delete!
+            "error": "Terminating for debugging purposes only."
+        })
+
+        return validated_data
+
     @transaction.atomic
     def create(self, validated_data):
         """
         Override the `create` function to add extra functinality.
         """
         # Extract the data we are processing.
-        slug = validated_data.get('member')
-        member = Member.objects.select_for_update().get(user__slug=slug)
-        request = self.context.get('request')
-        logger.info("Beginning promotion...")
+        role_id = validated_data.get('role_id')
 
-        # raise serializers.ValidationError({ # Uncomment when not using this code but do not delete!
-        #     "error": "Terminating for debugging purposes only."
-        # })
-
-        return validated_data
+        # Create the object based on the role assigned.
+        if role_id == SharedGroup.GROUP_MEMBERSHIP.AREA_COORDINATOR:
+            return self.create_area_coordinator(validated_data)
+        else:
+            return self.create_associate(validated_data)
