e9cfdb326df26d2d350fd74e6968333e34dbe794
bartmika <bart@mikasoftware.com>
2020-01-05 20:32:48 -0500

    Implemented more code to handle promoting staff members

--- nwapp/tenant_member/serializers/promote_operation_serializers.py
+++ nwapp/tenant_member/serializers/promote_operation_serializers.py
@@ -15,6 +15,7 @@ from rest_framework import exceptions, serializers
 from rest_framework.response import Response
 from rest_framework.validators import UniqueValidator
 
+from shared_foundation.constants import *
 from shared_foundation.models import SharedUser, SharedGroup
 from tenant_foundation.constants import *
 from tenant_foundation.models import (
@@ -22,7 +23,8 @@ from tenant_foundation.models import (
     Member,
     MemberComment,
     AreaCoordinator,
-    Associate
+    Associate,
+    Staff
 )
 
 
@@ -31,12 +33,23 @@ logger = logging.getLogger(__name__)
 
 class MemberPromoteOperationSerializer(serializers.Serializer):
     member = serializers.SlugField(write_only=True, required=True)
-    role_id = serializers.IntegerField(write_only=True, required=True,)
+    role_id = serializers.ChoiceField(
+        write_only=True,
+        required=True,
+        choices=(
+            (MANAGEMENT_GROUP_ID, _('Management')),
+            (FRONTLINE_GROUP_ID, _('Frontline')),
+            (ASSOCIATE_GROUP_ID, _('Associate')),
+            (AREA_COORDINATOR_GROUP_ID, _('Area Coordinator')),
+            (MEMBER_GROUP_ID, _('Member')),
+        )
+    )
     area_coordinator_agreement = serializers.BooleanField(write_only=True, required=True,)
     conflict_of_interest_agreement = serializers.BooleanField(write_only=True, required=True,)
     code_of_conduct_agreement = serializers.BooleanField(write_only=True, required=True,)
     confidentiality_agreement = serializers.BooleanField(write_only=True, required=True,)
     associate_agreement = serializers.BooleanField(write_only=True, required=True,)
+    staff_agreement = serializers.BooleanField(write_only=True, required=True,)
     police_check_date = serializers.DateField(write_only=True, required=True,)
 
     def create_area_coordinator(self, validated_data):
@@ -58,7 +71,7 @@ class MemberPromoteOperationSerializer(serializers.Serializer):
         confidentiality_agreement = render_to_string('account/confidentiality_agreement/2019_05_01.txt', {})
 
         # Create or update our model.
-        area_coordinator = AreaCoordinator.objects.update_or_create(
+        area_coordinator, created = AreaCoordinator.objects.update_or_create(
             user=member.user,
             defaults={
                 'user': member.user,
@@ -116,10 +129,10 @@ class MemberPromoteOperationSerializer(serializers.Serializer):
         associate_agreement = render_to_string('account/associate_agreement/2019_05_01.txt', {})
 
         # Create or update our model.
-        associate = Associate.objects.update_or_create(
-            member=member,
+        associate, created = Associate.objects.update_or_create(
+            user=user,
             defaults={
-                'member': member,
+                'user': member.user,
                 'has_signed_conflict_of_interest_agreement': True,
                 'conflict_of_interest_agreement': conflict_of_interest_agreement,
                 'conflict_of_interest_agreement_signed_on': timezone.now(),
@@ -149,20 +162,75 @@ class MemberPromoteOperationSerializer(serializers.Serializer):
 
         return member
 
+    def create_staff(self, validated_data):
+        slug = validated_data.get('member')
+        member = Member.objects.select_for_update().get(user__slug=slug)
+        request = self.context.get('request')
+        role_id = validated_data.get('role_id')
+        conflict_of_interest_agreement = validated_data.get('conflict_of_interest_agreement')
+        code_of_conduct_agreement = validated_data.get('code_of_conduct_agreement')
+        confidentiality_agreement = validated_data.get('confidentiality_agreement')
+        staff_agreement = validated_data.get('staff_agreement')
+        police_check_date = validated_data.get('police_check_date')
+
+        logger.info("Beginning promotion...")
+
+        # Get the text agreement which will be signed.
+        conflict_of_interest_agreement = render_to_string('account/conflict_of_interest_agreement/2019_05_01.txt', {})
+        code_of_conduct_agreement = render_to_string('account/code_of_conduct_agreement/2019_05_01.txt', {})
+        confidentiality_agreement = render_to_string('account/confidentiality_agreement/2019_05_01.txt', {})
+        staff_agreement = render_to_string('account/staff_agreement/2019_05_01.txt', {})
+
+        # Create or update our model.
+        staff, created = Staff.objects.update_or_create(
+            user=member.user,
+            defaults={
+                'user': member.user,
+                'has_signed_conflict_of_interest_agreement': True,
+                'conflict_of_interest_agreement': conflict_of_interest_agreement,
+                'conflict_of_interest_agreement_signed_on': timezone.now(),
+                'has_signed_code_of_conduct_agreement': True,
+                'code_of_conduct_agreement': code_of_conduct_agreement,
+                'code_of_conduct_agreement_signed_on': timezone.now(),
+                'has_signed_confidentiality_agreement': True,
+                'confidentiality_agreement': confidentiality_agreement,
+                'confidentiality_agreement_signed_on': timezone.now(),
+                'has_signed_staff_agreement': True,
+                # 'staff_agreement': staff_agreement,
+                # 'staff_agreement_signed_on': timezone.now(),
+                'police_check_date': police_check_date,
+            }
+        )
+
+        # Set the user's role to be a area coordinator after clearing the
+        # previous group memberships.
+        staff.user.groups.clear()
+        staff.user.groups.add(role_id)
+
+        # raise serializers.ValidationError({ # Uncomment when not using this code but do not delete!
+        #     "error": "Terminating for debugging purposes only."
+        # })
+
+        logger.info("Promoted to staff")
+
+        return staff
+
     @transaction.atomic
     def create(self, validated_data):
         """
         Override the `create` function to add extra functinality.
         """
+        # raise serializers.ValidationError({ # Uncomment when not using this code but do not delete!
+        #     "error": "Terminating for debugging purposes only."
+        # })
+
         # Extract the data we are processing.
         role_id = validated_data.get('role_id')
-
-        #TODO: MANAGEMENT STAFF
-        #TODO: FRONTLINE STAFF
-        #TODO: ASSOCIATE
 
         # Create the object based on the role assigned.
         if role_id == SharedGroup.GROUP_MEMBERSHIP.AREA_COORDINATOR:
             return self.create_area_coordinator(validated_data)
-        else:
+        elif role_id == SharedGroup.GROUP_MEMBERSHIP.ASSOCIATE:
             return self.create_associate(validated_data)
+        elif role_id == SharedGroup.GROUP_MEMBERSHIP.MANAGER or role_id == SharedGroup.GROUP_MEMBERSHIP.FRONTLINE_STAFF:
+            return self.create_staff(validated_data)
