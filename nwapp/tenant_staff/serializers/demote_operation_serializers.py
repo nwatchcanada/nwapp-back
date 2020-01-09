6306b07ed04082ff8412fa3e7223765b7a6bdb2d
bartmika <bart@mikasoftware.com>
2020-01-09 01:52:18 -0500

    BUGFIX: Fixed issue with demotions from staff.

--- nwapp/tenant_staff/serializers/demote_operation_serializers.py
+++ nwapp/tenant_staff/serializers/demote_operation_serializers.py
@@ -22,7 +22,9 @@ from tenant_foundation.models import (
     Staff,
     StaffComment,
     Staff,
-    Staff
+    Staff,
+    AreaCoordinator,
+    Associate
 )
 
 
@@ -67,16 +69,21 @@ class StaffDemoteOperationSerializer(serializers.Serializer):
         reason = int(validated_data.get('reason'))
         reason_other = validated_data.get('reason_other')
 
-        # For debugging purposes only.
-        logger.info(slug, role_id, reason, reason_other)
-        print(slug, role_id, reason, reason_other)
+        #---------------------------
+        # Reason for demotion.
+        #---------------------------
 
+        # Update demotion reason.
         staff.demotion_reason = reason
         staff.demotion_reason_other = reason_other
         staff.last_modified_by = request.user
         staff.last_modified_from = request.client_ip
         staff.last_modified_from_is_public = request.client_ip_is_routable
         staff.save()
+
+        #---------------------------
+        # Adjust group membership.
+        #---------------------------
 
         # Set the user's role to be a area coordinator after clearing the
         # previous group memberships.
@@ -100,6 +107,64 @@ class StaffDemoteOperationSerializer(serializers.Serializer):
             staff=staff,
             comment=comment,
         )
+
+        #---------------------------
+        # Adjust our profile.
+        #---------------------------
+        conflict_of_interest_agreement = render_to_string('account/conflict_of_interest_agreement/2019_05_01.txt', {})
+        code_of_conduct_agreement = render_to_string('account/code_of_conduct_agreement/2019_05_01.txt', {})
+        confidentiality_agreement = render_to_string('account/confidentiality_agreement/2019_05_01.txt', {})
+        if role_id == SharedGroup.GROUP_MEMBERSHIP.AREA_COORDINATOR:
+            area_coordinator_agreement = render_to_string('account/area_coordinator_agreement/2019_05_01.txt', {})
+            AreaCoordinator.objects.update_or_create(
+                user=staff.user,
+                defaults={
+                    'user': staff.user,
+                    'has_signed_area_coordinator_agreement': True,
+                    'area_coordinator_agreement': area_coordinator_agreement,
+                    'area_coordinator_agreement_signed_on': timezone.now(),
+                    'has_signed_conflict_of_interest_agreement': True,
+                    'conflict_of_interest_agreement': conflict_of_interest_agreement,
+                    'conflict_of_interest_agreement_signed_on': timezone.now(),
+                    'has_signed_code_of_conduct_agreement': True,
+                    'code_of_conduct_agreement': code_of_conduct_agreement,
+                    'code_of_conduct_agreement_signed_on': timezone.now(),
+                    'has_signed_confidentiality_agreement': True,
+                    'confidentiality_agreement': confidentiality_agreement,
+                    'confidentiality_agreement_signed_on': timezone.now(),
+                    # 'police_check_date': police_check_date,
+                    'created_by': request.user,
+                    'created_from': request.client_ip,
+                    'created_from_is_public': request.client_ip_is_routable,
+                    'last_modified_by': request.user,
+                    'last_modified_from': request.client_ip,
+                    'last_modified_from_is_public': request.client_ip_is_routable,
+                }
+            )
+        elif role_id == SharedGroup.GROUP_MEMBERSHIP.ASSOCIATE:
+            associate_agreement = render_to_string('account/associate_agreement/2019_05_01.txt', {})
+            Associate.objects.update_or_create(
+                user=staff.user,
+                defaults={
+                    'user': staff.user,
+                    'has_signed_conflict_of_interest_agreement': True,
+                    'conflict_of_interest_agreement': conflict_of_interest_agreement,
+                    'conflict_of_interest_agreement_signed_on': timezone.now(),
+                    'has_signed_code_of_conduct_agreement': True,
+                    'code_of_conduct_agreement': code_of_conduct_agreement,
+                    'code_of_conduct_agreement_signed_on': timezone.now(),
+                    'has_signed_confidentiality_agreement': True,
+                    'confidentiality_agreement': confidentiality_agreement,
+                    'confidentiality_agreement_signed_on': timezone.now(),
+                    'has_signed_associate_agreement': True,
+                    'associate_agreement': associate_agreement,
+                    'associate_agreement_signed_on': timezone.now(),
+                    # 'police_check_date': police_check_date,
+                }
+            )
+        elif role_id == SharedGroup.GROUP_MEMBERSHIP.MEMBER:
+            pass
+
 
         # raise serializers.ValidationError({ # Uncomment when not using this code but do not delete!
         #     "error": "Terminating for debugging purposes only."
