dc88277dbc0e9a3da5dafb53e1f79d2b16cbab56
bartmika <bart@mikasoftware.com>
2020-01-16 21:53:33 -0500

    BUGFIX: Implemented unique name validator.

--- nwapp/tenant_foundation/serializers/district/create_serializer.py
+++ nwapp/tenant_foundation/serializers/district/create_serializer.py
@@ -15,8 +15,8 @@ from rest_framework.response import Response
 from rest_framework.validators import UniqueValidator
 
 from shared_foundation.models import SharedUser
-from tenant_foundation.models import District
-
+from shared_foundation.utils import get_content_file_from_base64_string # REACT-DJANGO UPLOAD | STEP 2 OF 4
+from tenant_foundation.models import District, PrivateImageUpload
 
 logger = logging.getLogger(__name__)
 
@@ -33,6 +33,11 @@ class DistrictCreateSerializer(serializers.Serializer):
         allow_blank=True,
         allow_null=True,
         write_only=True,
+        validators=[
+            UniqueValidator(
+                queryset=District.objects.all(),
+            )
+        ],
     )
     description = serializers.CharField(
         required=False,
@@ -64,7 +69,19 @@ class DistrictCreateSerializer(serializers.Serializer):
         allow_null=True,
         write_only=True,
     )
-    # logo_image
+
+    # REACT-DJANGO UPLOAD | STEP 1 OF 4: We define two string fields required (write-only)
+    # for accepting our file uploads.
+    upload_content = serializers.CharField(
+        write_only=True,
+        allow_null=False,
+        required=False,
+    )
+    upload_filename = serializers.CharField(
+        write_only=True,
+        allow_null=False,
+        required=False,
+    )
 
     def create(self, validated_data):
         """
@@ -79,6 +96,32 @@ class DistrictCreateSerializer(serializers.Serializer):
         counselor_phone = validated_data.get('counselor_phone')
         website_url = validated_data.get('website_url')
 
+        try:
+            # Extract our upload file data
+            content = validated_data.get('upload_content')
+            filename = validated_data.get('upload_filename')
+            if settings.DEBUG:
+                filename = "QA_"+filename # NOTE: Attach `QA_` prefix if server running in QA mode.
+            content_file = get_content_file_from_base64_string(content, filename) # REACT-DJANGO UPLOAD | STEP 3 OF 4: Convert to `ContentFile` type.
+
+            # Create our file.
+            private_file = PrivateImageUpload.objects.create(
+                title = "District Business Logo",
+                description = "-",
+                is_archived = False,
+                # user = user,
+                data_file = content_file, # REACT-DJANGO UPLOAD | STEP 4 OF 4: When you attack a `ContentImage`, Django handles all file uploading.
+                created_by = request.user,
+                created_from = request.client_ip,
+                created_from_is_public = request.client_ip_is_routable,
+                last_modified_by = request.user,
+                last_modified_from = request.client_ip,
+                last_modified_from_is_public = request.client_ip_is_routable,
+            )
+        except Exception as e:
+            private_file = None
+
+        # Create the district.
         district = District.objects.create(
             type_of=type_of,
             description=description,
@@ -87,6 +130,7 @@ class DistrictCreateSerializer(serializers.Serializer):
             counselor_email=counselor_email,
             counselor_phone=counselor_phone,
             website_url=website_url,
+            logo_image=private_file,
         )
 
         logger.info("New district was been created.")
