7d67905eaa665b09b87d1f37b13e29145e92a502
bartmika <bart@mikasoftware.com>
2020-03-02 22:57:20 -0500

    Uploaded resource image saving. https://github.com/nwatchcanada/nwapp-front/issues/263

--- nwapp/tenant_item/serializers/item/update/resource_details_update_serializer.py
+++ nwapp/tenant_item/serializers/item/update/resource_details_update_serializer.py
@@ -14,7 +14,7 @@ from rest_framework.response import Response
 from rest_framework.validators import UniqueValidator
 
 from shared_foundation.utils import get_content_file_from_base64_string
-from tenant_foundation.models import Item, ItemType, PrivateImageUpload
+from tenant_foundation.models import Item, ItemType, PrivateImageUpload, PrivateFileUpload
 
 
 logger = logging.getLogger(__name__)
@@ -38,6 +38,72 @@ class ResourceDetailsUpdateSerializer(serializers.Serializer):
         required=False,
         allow_null=True,
     )
+    resource_image = serializers.JSONField(
+       required=False,
+       allow_null=True,
+    )
+    resource_file = serializers.JSONField(
+       required=False,
+       allow_null=True,
+    )
+
+    def create_resource_image(self, request, validated_data):
+        try:
+            # Extract our upload file data
+            resource_image = validated_data.get('resource_image', None)
+            content = resource_image.get('data', None)
+            filename = resource_image.get('file_name', None)
+            if settings.DEBUG:
+                filename = "QA_"+filename # NOTE: Attach `QA_` prefix if server running in QA mode.
+            content_file = get_content_file_from_base64_string(content, filename) # REACT-DJANGO UPLOAD | STEP 3 OF 4: Convert to `ContentFile` type.
+
+            # Create our file.
+            private_file = PrivateImageUpload.objects.create(
+                is_archived = False,
+                user = request.user,
+                image_file = content_file, # REACT-DJANGO UPLOAD | STEP 4 OF 4: When you attack a `ContentImage`, Django handles all file uploading.
+                created_by = request.user,
+                created_from = request.client_ip,
+                created_from_is_public = request.client_ip_is_routable,
+                last_modified_by = request.user,
+                last_modified_from = request.client_ip,
+                last_modified_from_is_public = request.client_ip_is_routable,
+            )
+            logger.info("Private image was been created.")
+            return private_file
+        except Exception as e:
+            print(e)
+            private_file = None
+            return None
+
+    def create_resource_file(self, request, validated_data):
+        try:
+            # Extract our upload file data
+            resource_file = validated_data.get('resource_file', None)
+            content = resource_file.get('data', None)
+            filename = resource_file.get('file_name', None)
+            if settings.DEBUG:
+                filename = "QA_"+filename # NOTE: Attach `QA_` prefix if server running in QA mode.
+            content_file = get_content_file_from_base64_string(content, filename) # REACT-DJANGO UPLOAD | STEP 3 OF 4: Convert to `ContentFile` type.
+
+            # Create our file.
+            private_file = PrivateFileUpload.objects.create(
+                is_archived = False,
+                user = request.user,
+                data_file = content_file, # REACT-DJANGO UPLOAD | STEP 4 OF 4: When you attack a `ContentImage`, Django handles all file uploading.
+                created_by = request.user,
+                created_from = request.client_ip,
+                created_from_is_public = request.client_ip_is_routable,
+                last_modified_by = request.user,
+                last_modified_from = request.client_ip,
+                last_modified_from_is_public = request.client_ip_is_routable,
+            )
+            logger.info("Private file was been created.")
+            return private_file
+        except Exception as e:
+            print(e)
+            private_file = None
+            return None
 
     def update(self, instance, validated_data):
         """
@@ -48,6 +114,18 @@ class ResourceDetailsUpdateSerializer(serializers.Serializer):
         description = validated_data.get('description')
         external_url = validated_data.get('external_url')
         embed_code = validated_data.get('embed_code')
+        resource_image = validated_data.get('resource_image')
+        resource_file = validated_data.get('resource_file')
+
+        # Attach the file upload.
+        if instance.format_type == Item.FORMAT_TYPE.IMAGE_RESOURCE_TYPE_OF:
+            if instance.resource_image:
+                instance.resource_image.delete()
+            instance.resource_image = self.create_resource_image(request, validated_data)
+        elif instance.format_type == Item.FORMAT_TYPE.FILE_RESOURCE_TYPE_OF:
+            if instance.resource_file:
+                instance.resource_file.delete()
+            instance.resource_file = self.create_resource_file(request, validated_data)
 
         # Save it.
         instance.title = title
