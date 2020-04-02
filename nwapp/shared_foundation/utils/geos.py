c05a27a1112e6d3939f7491b2d9003d02efabf16
bartmika <bart@mikasoftware.com>
2020-04-01 21:23:36 -0400

    BUGFIX: Added more defensive code.

--- nwapp/shared_foundation/utils/geos.py
+++ nwapp/shared_foundation/utils/geos.py
@@ -26,10 +26,13 @@ def get_point_from_dict(dict):
     INPUT: {'longitude': -81.249722, 'latitude': 42.983611}
     OUTPUT: Point(42.983611, 81.249722)
     """
-    return Point(
-        float(dict['latitude']),
-        float(dict['longitude'])
-    )
+    try:
+        return Point(
+            float(dict['latitude']),
+            float(dict['longitude'])
+        )
+    except Exception as e:
+        return None
 
 
 def get_point_from_arr(arr):
@@ -40,10 +43,13 @@ def get_point_from_arr(arr):
     INPUT: [42.983611, -81.249722]
     OUTPUT: Point(42.983611, 81.249722)
     """
-    return Point(
-        float(arr[0]),
-        float(arr[1])
-    )
+    try:
+        return Point(
+            float(arr[0]),
+            float(arr[1])
+        )
+    except Exception as e:
+        return None
 
 def get_arr_from_point(point):
     """
@@ -70,10 +76,13 @@ def get_polygon_from_multi_arr(arr):
     INPUT: [[42.989572, -81.23488], [42.976001, -81.233335], [42.97776, -81.208265], [42.989572, -81.23488]]
     OUTPUT: Polygon( ((42.99711 -81.275747, 42.975624 -81.30459500000001, 42.969088 -81.30116, 42.966826 -81.26733299999999, 42.971476 -81.22938499999999, 42.988064 -81.1947, 43.000628 -81.222002, 42.99711 -81.275747))
     """
-    lines = []
-    for coord in arr:
-        lines.append( (coord[0],coord[1]) )
-    return Polygon(lines)
+    try:
+        lines = []
+        for coord in arr:
+            lines.append( (coord[0],coord[1]) )
+        return Polygon(lines)
+    except Exception as e:
+        return None
 
 
 def get_multi_arr_from_polygon(polygon):
@@ -85,12 +94,15 @@ def get_multi_arr_from_polygon(polygon):
     INPUT: Polygon( ((42.99711 -81.275747, 42.975624 -81.30459500000001, 42.969088 -81.30116, 42.966826 -81.26733299999999, 42.971476 -81.22938499999999, 42.988064 -81.1947, 43.000628 -81.222002, 42.99711 -81.275747))
     OUTPUT: [[42.989572, -81.23488], [42.976001, -81.233335], [42.97776, -81.208265], [42.989572, -81.23488]]
     """
-    points_arr = []
-    for linear_ring in polygon:
-        linear_ring_tupile = linear_ring.array
-        for line_point in linear_ring_tupile:
-            points_arr.append([
-                line_point[0],
-                line_point[1]
-            ])
-    return [points_arr]
+    try:
+        points_arr = []
+        for linear_ring in polygon:
+            linear_ring_tupile = linear_ring.array
+            for line_point in linear_ring_tupile:
+                points_arr.append([
+                    line_point[0],
+                    line_point[1]
+                ])
+        return [points_arr]
+    except Exception as e:
+        return None
