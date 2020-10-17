5d7a3a7b0777492cf95bcd8f9d989246e3390623
bartmika <bart@mikasoftware.com>
2020-10-17 15:56:13 -0400

    Implement report 8 - https://github.com/nwatchcanada/nwapp-front/issues/288

---
+++ nwapp/tenant_report/views/csv/report_08_view.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import datetime
+from dateutil import parser
+from djmoney.money import Money
+from django.contrib.auth.decorators import login_required
+from django.db.models.functions import Extract
+from django.db.models import Q
+from django.views.generic import DetailView, ListView, TemplateView
+from django.utils.decorators import method_decorator
+from django.utils.translation import ugettext_lazy as _
+from django.utils import timezone
+
+from shared_foundation.constants import *
+from shared_foundation.utils import *
+from tenant_foundation.constants import *
+from tenant_foundation.models import (
+    Watch, Associate, AreaCoordinator, Member
+)
+
+"""
+Code below was taken from:
+https://docs.djangoproject.com/en/2.0/howto/outputting-csv/
+"""
+
+import csv
+from django.http import StreamingHttpResponse
+
+class Echo:
+    """An object that implements just the write method of the file-like
+    interface.
+    """
+    def write(self, value):
+        """Write the value by returning it, instead of storing in a buffer."""
+        return value
+
+
+def report_08_streaming_csv_view(request):
+    today = timezone.now()
+    watches = Watch.objects.filter(Q(governors=None)).order_by("id")
+
+    # Convert our aware datetimes to the specific timezone of the tenant.
+    tenant_today = request.tenant.to_tenant_dt(today)
+
+    # Generate our new header.
+    rows = (["Unassignment Watch Report","","","",],)
+    rows += (["Report Date:", pretty_dt_string(tenant_today),"", "",],)
+    rows += (["", "", "", "", "",],)
+    rows += (["", "", "", "",],)
+
+    # Generate the CSV header row.
+    rows += ([
+        "Name",
+        "Associate Count",
+        "Member Count",
+        "Tags"
+    ],)
+
+    # Generate the CSV dataset.
+    for watch in watches.all():
+
+        a_count = Associate.objects.filter(
+            Q(watch=watch)|
+            Q(user__area_coordinator__watch=watch)|
+            Q(user__member__watch=watch)
+        ).count()
+        m_count = Member.objects.filter(watch=watch).count()
+
+        # Generate our row.
+        rows += ([
+            watch.name,
+            a_count,
+            m_count,
+        ],)
+
+    pseudo_buffer = Echo()
+    writer = csv.writer(pseudo_buffer)
+    response = StreamingHttpResponse(
+        (writer.writerow(row) for row in rows),
+        content_type="text/csv"
+    )
+    response['Content-Disposition'] = 'attachment; filename="unassigned_watches.csv"'
+    return response
